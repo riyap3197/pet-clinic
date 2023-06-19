4fb34d95d244938b2029811993e482c2bebf99f2
riyap3197 <riyap3197@gmail.com>
2023-06-19 17:10:08 -0400

    updated vet to use JPA entities. Closes #33.

--- pet-clinic-data/src/main/java/sprinkframework/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/sprinkframework/petclinic/model/Speciality.java
@@ -1,7 +1,14 @@
 package sprinkframework.petclinic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
