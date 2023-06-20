e8a32c21069bdcdead99e188bec6088bb689a6ad
riyap3197 <riyap3197@gmail.com>
2023-06-19 21:20:17 -0400

    added Project Lombok to project. Closes #46.

--- pet-clinic-data/src/main/java/sprinkframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/sprinkframework/petclinic/model/Owner.java
@@ -1,13 +1,27 @@
 package sprinkframework.petclinic.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person{
+
+    @Builder
+    public Owner(Long id, String  firstName, String lastName, String address, String city,
+                 String telephone, Set<Pet> pet) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        this.pet = pet;
+    }
 
     @Column(name = "address")
     private String address;
@@ -21,35 +35,4 @@ public class Owner extends Person{
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pet = new HashSet<>();
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPet() {
-        return pet;
-    }
-
-    public void setPet(Set<Pet> pet) {
-        this.pet = pet;
-    }
 }
